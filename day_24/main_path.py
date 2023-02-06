4cdb8a38eef807bb4310f4607da27d14632c8279
Dmitriy Sorokin <sorokin.da92@gmail.com>
2023-02-06 22:58:24 +0300

    day 24

--- day_24/main_path.py
+++ day_24/main_path.py
@@ -11,14 +11,3 @@ with open(r"..\day_24\Input\Letters\starting_letter.txt", mode="r") as letter:
         print(new_letter)
         with open(fr".\Output\ReadyToSend\letter_for_{stript_name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
-
-# for names in data:
-#     with open(fr"..\day_24\Output\ReadyToSend\letter_for_{names}.txt", mode="r") as new_letters:
-#         pass
-#         print(new_letters.read())
-#         # x = new_letters.write(n_letter)
-#         # print(letter)
-
-# with open(r"..\day_24\Input\Letters\starting_letter.txt", mode="a") as letter:
-#     letter.read().replace("[name]", data[0])
-#     print(letter)
