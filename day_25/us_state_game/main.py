914f02b05c159bdff79d12d4515c6aba78534b84
Dmitriy Sorokin <sorokin.da92@gmail.com>
2023-02-07 22:10:42 +0300

    day 25

--- day_25/us_state_game/main.py
+++ day_25/us_state_game/main.py
@@ -2,7 +2,7 @@ import turtle
 from turtle import Screen, Turtle
 import pandas
 
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 12, "normal")
 ALIGNMENT = "center"
 
 screen = Screen()
@@ -12,25 +12,29 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
-data_l = data["state"]
 
-coor = data[data.state == "Texas"]
-coor_l = coor.to_dict()
-print(coor_l)
-score = 0
-
-go_next = True
-while go_next:
-    answer_state = screen.textinput(title=f"{score}/{len(data)} State Correct",
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/{len(data)} State Correct",
                                     prompt="Whats another state's name?")
     answer_state_lower = answer_state.title()
     print(answer_state_lower)
     for state in data.state:
         if answer_state_lower == state:
-            score += 1
+            guessed_states.append(answer_state_lower)
             state_turtl = Turtle()
             coor = data[data.state == state]
             coor_dict = coor.to_dict()
-            state_turtl.goto(coor_dict["x"][42], coor_dict["y"][42])
+            for i in coor_dict:
+                if i == "x":
+                    for key in coor_dict[i]:
+                        new_coor_x = coor_dict[i][key]
+                        print(new_coor_x)
+                elif i == "y":
+                    for key in coor_dict[i]:
+                        new_coor_y = coor_dict[i][key]
+                        print(new_coor_y)
+            state_turtl.hideturtle()
+            state_turtl.penup()
+            state_turtl.goto(new_coor_x, new_coor_y)
             state_turtl.write(arg=state, move=False, align=ALIGNMENT, font=FONT)
-            print("TRRR")
