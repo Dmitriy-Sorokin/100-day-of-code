4b95499d841cd6b7fa1f929fc5007ca12de46091
Dmitriy Sorokin <sorokin.da92@gmail.com>
2023-02-10 18:17:04 +0300

    day 27

---
+++ day_27/converter.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+window = Tk()
+window.minsize(height=200, width=300)
+
+entry = Entry()
+entry.grid(column=1, row=0)
+
+l_miles = Label(text="Miles")
+l_miles.grid(column=2, row=0)
+l_miles.config(pady=20)
+
+l_equal = Label(text="is equal to")
+l_equal.grid(column=0, row=1)
+l_equal.config(padx=20)
+
+l_num_km = Label(text="0")
+l_num_km.grid(column=1, row=1)
+l_num_km.config(pady=10)
+
+l_km = Label(text='Km')
+l_km.grid(column=2, row=1)
+
+
+def calculate():
+    mil = entry.get()
+    km = int(mil) * 1.609
+    l_num_km.config(text=int(km))
+
+
+b_calculate = Button(text="Calculate", command=calculate)
+b_calculate.grid(column=1, row=2)
+
+window.mainloop()
--- day_27/playground.py
+++ day_27/playground.py
@@ -1,16 +1,37 @@
+# *args: Positional Variable-Length Arguments
 def add(*args):
-    total_value = 0
-    for num in args:
-        total_value += num
-    return total_value
+    # print(args[1])
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
 
 
-# print(add(3, 5, 6))
+# print(add(3, 5, 6, 2, 1, 7, 4, 3))
 
-
+# **kwargs: Keyworded Variable-Length Arguments
 def calculate(n, **kwargs):
-    n *= kwargs["add"]
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
     n *= kwargs["multiply"]
-    print(n)
+    # print(n)
+
 
 calculate(2, add=3, multiply=5)
+
+
+# How to use a **kwargs dictionary safely
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="Skyline")
+print(my_car.model)
