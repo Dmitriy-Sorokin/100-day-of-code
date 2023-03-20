d66d06f4e98d8cca2b6fb087e4548f7e0169a21d
Dmitriy Sorokin <sorokin.da92@gmail.com>
2023-03-20 22:34:42 +0300

    day 54

--- day_53/main_53.py
+++ day_53/main_53.py
@@ -19,11 +19,13 @@ class Forms:
         self.driver.get(
             "https://r.onliner.by/ak/?price%5Bmin%5D=50&price%5Bmax%5D=320&currency=usd&rent_type%5B%5D=2_rooms&only_owner=true#bounds%5Blb%5D%5Blat%5D=53.909495557432045&bounds%5Blb%5D%5Blong%5D=27.5562858581543&bounds%5Brt%5D%5Blat%5D=53.933176030067045&bounds%5Brt%5D%5Blong%5D=27.597141265869144")
         ads = self.driver.find_elements(By.CSS_SELECTOR,
-                                        "#search-filter-results > div.classifieds > div > div.classifieds-list")
-        count = 0
-        for i in ads:
-            print(f"Квартира номер {count + 1}: {i.text}")
-        # print(ads)
+                                        "#search-filter-results > div.classifieds > div > div.classifieds-list a")
+        link = [item.get_attribute("href") for item in ads]
+        print(link)
+
+        apart = [kw.text.replace("\n", "").replace("2к", "").replace(" ", "").replace("$", " ").replace("р.", " ").split() for kw in ads]
+        print(apart)
+
 
 
 kw = Forms()
