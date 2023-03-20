4fba79b815974235cc9babd4f7158876fc7497c9
Dmitriy Sorokin <sorokin.da92@gmail.com>
2023-03-20 19:23:30 +0300

    day 53

---
+++ day_53/main_53.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+
+
+class Forms:
+
+    def __init__(self):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.maximize_window()
+
+    def forms_google(self):
+        url = "https://docs.google.com/forms/d/e/1FAIpQLSdzuWkUyTZnazCeD0_mRJgFHOD52C0pC33HDpay0Up4DxSa3Q/viewform?usp=sf_link"
+        pass
+
+    def onliner(self):
+        self.driver.get(
+            "https://r.onliner.by/ak/?price%5Bmin%5D=50&price%5Bmax%5D=320&currency=usd&rent_type%5B%5D=2_rooms&only_owner=true#bounds%5Blb%5D%5Blat%5D=53.909495557432045&bounds%5Blb%5D%5Blong%5D=27.5562858581543&bounds%5Brt%5D%5Blat%5D=53.933176030067045&bounds%5Brt%5D%5Blong%5D=27.597141265869144")
+        ads = self.driver.find_elements(By.CSS_SELECTOR,
+                                        "#search-filter-results > div.classifieds > div > div.classifieds-list")
+        count = 0
+        for i in ads:
+            print(f"Квартира номер {count + 1}: {i.text}")
+        # print(ads)
+
+
+kw = Forms()
+kw.onliner()
