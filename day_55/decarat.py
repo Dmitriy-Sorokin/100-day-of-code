0e3bd871b9cf6209501d4bce5a43fff248975af2
Dmitriy Sorokin <sorokin.da92@gmail.com>
2023-03-24 16:12:45 +0300

    day 55

--- day_55/decarat.py
+++ day_55/decarat.py
@@ -7,7 +7,7 @@ def img_src(func):
 
 def h_teg(func):
     def wrapper(*args, **kwargs):
-        return f'<h1>{func(*args, **kwargs)}</h1?'
+        return f'<h1>{func(*args, **kwargs)}</h1>'
 
     return wrapper
 
@@ -16,4 +16,15 @@ def u_teg(func):
     def wrapper(*args, **kwargs):
         return f'<u>{func(*args, **kwargs)}</u?'
 
-    return wrapper
+
+def decorator_project_h_l(func):
+    def wrapper_inner(number):
+        result = func(number)
+        if result == "<h1>You found me!</h1>":
+            return f"{result}<br><img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'>"
+        elif result == "<h1>Too high, try again!</h1>":
+            return f"{result}<br><img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'>"
+        else:
+            return f"{result}<br><img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'>"
+
+    return wrapper_inner
---
+++ day_55/ligher_lower.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from decarat import *
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@h_teg
+@img_src
+def hello_world():
+    return 'Guess a number between 0 and 9'
+
+@app.route("/<int:number>")
+@decorator_project_h_l
+@h_teg
+def greet(number: int):
+    random_number =  2 #random.randint(1, 10)
+    if random_number == number:
+        return "You found me!"
+    elif number > random_number:
+        return "Too high, try again!"
+    else:
+        return "Too low, try again!"
+
+print(greet(3))
+if __name__ == "__main__":
+    app.run(debug=True)
