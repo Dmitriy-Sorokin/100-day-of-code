2fb87b20f406c655286b6c0ab54cca645acdabc7
Dmitriy Sorokin <sorokin.da92@gmail.com>
2023-04-06 22:28:45 +0300

    day 63

--- day_63/main.py
+++ day_63/main.py
@@ -1,7 +1,45 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
 
 app = Flask(__name__)
 
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+with app.app_context():
+    db.create_all()
+
+# CREATE RECORD
+    new_book = Book(title="Harry Potter", author="J. K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
+
+all_books = Book.query.all()
+print(all_books)
 all_books = []
 
 
@@ -10,7 +48,9 @@ def home():
     fin = ""
     for dict_books in all_books:
         for items in dict_books:
-            fin += dict_books[items] + "/10" + "<br>"
+            fin += dict_books[items]
+    if fin == "":
+        return render_template("index.html", f="Library is empty")
 
     return render_template("index.html", f=fin)
 
