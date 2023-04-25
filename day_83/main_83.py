7b1823af4d549c30b17da695262d6c59ea327f90
Dmitriy Sorokin <sorokin.dm@promail366.com>
2023-04-25 21:36:22 +0300

    day_83

--- day_83/main_83.py
+++ day_83/main_83.py
@@ -9,7 +9,7 @@ def board_example():
     print(' 7 | 8 | 9 ')
 
 
-board_user = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 
 def print_board(board):
     print(f' {board[0]} | {board[1]} | {board[2]}  ')
@@ -18,29 +18,71 @@ def print_board(board):
     print('-----------')
     print(f' {board[6]} | {board[7]} | {board[8]}  ')
 
-print("Example board. In lead a number to select a location: ")
-board_example()
 
-next_step = False
-while not next_step:
-    try:
-        choose_user_1 = int(input("On the example board, look at where you want to put the cross and enter that number player word(X): "))
-    except ValueError:
-        print("Only number example board 1-9")
-        continue
-    print("Games board")
-    board_user[choose_user_1 - 1] = "X"
-    print_board(board=board_user)
-    next_step = checker_win(board=board_user)
-    if next_step:
-        break
-    try:
-        choose_user_2 = int(input("On the example board, look at where you want to put the cross and enter that number player word(0): "))
-    except ValueError:
-        print("Only number example board 1-9")
-        continue
-    print("Games board")
-    board_user[choose_user_2 - 1] = "0"
-    print_board(board=board_user)
-    checker_win(board=board_user)
 
+
+def logic_game():
+    print("Example board. In lead a number to select a location: ")
+    board_example()
+    board_user = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    next_step = False
+    while not next_step:
+        try:
+            while True:
+                choose_user_1 = int(input("On the example board, look at where you want to put the cross and enter that number player word(X): "))
+                if board_user[choose_user_1 - 1] == " ":
+                    board_user[choose_user_1 - 1] = "X"
+                    break
+                else:
+                    print("Choose different place")
+        except ValueError:
+            print("Only number example board 1-9")
+            continue
+        except IndexError:
+            print("Only number example board 1-9")
+            continue
+        print("Games board")
+
+        print_board(board=board_user)
+        next_step = checker_win(board=board_user)
+        if next_step:
+            while True:
+                more = input("Do you want more game? yes or no: ").lower()
+                if more == "yes":
+                    logic_game()
+                elif more == "no":
+                    print("See you later")
+                    break
+                else:
+                    print("Pleas write correctly word!!!")
+
+        while True:
+            try:
+                choose_user_2 = int(input("On the example board, look at where you want to put the cross and enter that number player word(0): "))
+                if board_user[choose_user_2 - 1] == " ":
+                    board_user[choose_user_2 - 1] = "0"
+                    break
+                else:
+                    print("Choose different place")
+            except ValueError:
+                print("Only number example board 1-9")
+                continue
+            except IndexError:
+                print("Only number example board 1-9")
+                continue
+
+        print("Games board")
+        print_board(board=board_user)
+        next_step = checker_win(board=board_user)
+        if next_step:
+            while True:
+                more = input("Do you want more game? yes or no: ").lower()
+                if more == "yes":
+                    logic_game()
+                elif more == "no":
+                    print("See you later")
+                    break
+                else:
+                    print("Pleas write correctly word!!!")
+
+logic_game()
